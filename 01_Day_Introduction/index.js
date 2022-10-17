f03e1e04641f10a25c9658161256e237269922a1
Saqib <iamsaqib24@gmail.com>
2022-10-17 11:36:29 +0530

    01_day_done

--- 01_Day_Introduction/index.js
+++ 01_Day_Introduction/index.js
@@ -8,3 +8,11 @@ console.log(typeof NaN)
 console.log(typeof undefined)
 console.log(typeof [ ])
 console.log(typeof { })
+
+
+// Variables Declaring & Assigning:
+
+let fullName = "Saqib"
+let age = 26
+let myCountry = "India"
+console.log(`My name is ${fullName}, I am ${age} years old. And I am from ${myCountry}.`);
