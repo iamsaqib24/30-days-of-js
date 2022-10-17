73782942f185374f760bad175ad14008bb72994f
Saqib <iamsaqib24@gmail.com>
2022-10-17 12:22:16 +0530

    math objects

---
+++ 02_Day_Data_types/index.js
@@ -0,0 +1,42 @@
+// Primitive & Non-Primitive Data Types:
+
+// Primitive Data Types: (immutable)
+// (Numbers, String, Booleans, null, undefined, Symbols)
+
+let word = "JavaScript"
+console.log(word)   // JavaScript
+word[0] = "Y"       // Won't work/change
+console.log(word)   // JavaScript
+
+// Non-Primitive Data Types: (Arrays, Objects)
+// It cannot be compared by value, because they are being compared by reference instead of value.
+
+let arr = ["Apple", "OnePlus", "Samsung", "Pixel"]
+
+arr[3] = "Google"
+
+console.log(arr)
+
+let arr1 = ["Apple", "OnePlus", "Samsung", "Pixel"]
+
+console.log(arr == arr1)        // false
+
+let arr2 = arr
+
+console.log(arr == arr2)        // true
+
+
+// Math Objects:
+
+const gravity = 9.81
+const PI = Math.PI
+console.log(PI)                     // 3.141592
+console.log(Math.round(PI))         // 3
+console.log(Math.round(gravity))    // 10
+console.log(Math.floor(PI))         // 3 rounding down
+console.log(Math.ceil(PI))          // 4 rounding up
+console.log(Math.min(20, 30, -10, -25, 0, 50))      // -25
+console.log(Math.max(20, 30, -10, -25, 0, 50))      // 50
+// generate random numbers form 0 to 0.999999
+const randomNum = Math.floor(Math.random() * 11)
+console.log(randomNum)
