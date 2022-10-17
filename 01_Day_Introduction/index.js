882d28ac6225bab68e6a159e337a2abff2acfe95
Saqib <iamsaqib24@gmail.com>
2022-10-17 11:24:09 +0530

    introduction

---
+++ 01_Day_Introduction/index.js
@@ -0,0 +1,10 @@
+// Checking Data Types:
+
+console.log(typeof 5)
+console.log(typeof 'Saqib')
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof NaN)
+console.log(typeof undefined)
+console.log(typeof [ ])
+console.log(typeof { })
