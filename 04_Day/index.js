9470a192d7f74305cdbf16d0ca206f47a4c35535
Saqib <iamsaqib24@gmail.com>
2022-10-19 11:02:32 +0530

    conditional statements

---
+++ 04_Day/index.js
@@ -0,0 +1,48 @@
+console.log("04-Day");
+
+// Conditional Statements:
+
+// if else if else
+let weather = 'rainy'
+if (weather === 'rainy') {
+  console.log('You need a rain coat.')
+} else if (weather === 'cloudy') {
+  console.log('It might be cold, you need a jacket.')
+} else if (weather === 'sunny') {
+  console.log('Go out freely.')
+} else {
+  console.log('No need for rain coat.')
+}
+
+
+// Switch statement,
+
+let dayUserInput = prompt("What day is today ?");
+let day = dayUserInput.toLowerCase();
+
+switch (day) {
+    case 'monday':
+            console.log("Today is Monday");
+        break;
+    case 'tuesday':
+        console.log("Today is Tuesday");
+        break;
+    case 'wednesday':
+        console.log("Today is Wednesday");
+        break;
+    case 'thursday':
+        console.log("Today is Thursday");
+        break;
+    case 'friday':
+        console.log("Today is Friday");
+        break;
+    case 'saturday':
+        console.log("Today is Saturday");
+        break;
+    case 'Sunday':
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("It is not a week day.");
+        break;
+}
