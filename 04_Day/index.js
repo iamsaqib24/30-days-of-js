89ad816d7b966a178c53139eea9c26fd02b2ffbc
Saqib <iamsaqib24@gmail.com>
2022-10-19 11:06:41 +0530

    ternary operator

--- 04_Day/index.js
+++ 04_Day/index.js
@@ -3,7 +3,7 @@ console.log("04-Day");
 // Conditional Statements:
 
 // if else if else
-let weather = 'rainy'
+let weather = 'sunny'
 if (weather === 'rainy') {
   console.log('You need a rain coat.')
 } else if (weather === 'cloudy') {
@@ -17,32 +17,40 @@ if (weather === 'rainy') {
 
 // Switch statement,
 
-let dayUserInput = prompt("What day is today ?");
-let day = dayUserInput.toLowerCase();
+// let dayUserInput = prompt("What day is today ?");
+// let day = dayUserInput.toLowerCase();
 
-switch (day) {
-    case 'monday':
-            console.log("Today is Monday");
-        break;
-    case 'tuesday':
-        console.log("Today is Tuesday");
-        break;
-    case 'wednesday':
-        console.log("Today is Wednesday");
-        break;
-    case 'thursday':
-        console.log("Today is Thursday");
-        break;
-    case 'friday':
-        console.log("Today is Friday");
-        break;
-    case 'saturday':
-        console.log("Today is Saturday");
-        break;
-    case 'Sunday':
-        console.log("Today is Sunday");
-        break;
-    default:
-        console.log("It is not a week day.");
-        break;
-}
+// switch (day) {
+//     case 'monday':
+//             console.log("Today is Monday");
+//         break;
+//     case 'tuesday':
+//         console.log("Today is Tuesday");
+//         break;
+//     case 'wednesday':
+//         console.log("Today is Wednesday");
+//         break;
+//     case 'thursday':
+//         console.log("Today is Thursday");
+//         break;
+//     case 'friday':
+//         console.log("Today is Friday");
+//         break;
+//     case 'saturday':
+//         console.log("Today is Saturday");
+//         break;
+//     case 'Sunday':
+//         console.log("Today is Sunday");
+//         break;
+//     default:
+//         console.log("It is not a week day.");
+//         break;
+// }
+
+
+// Ternary Operator,
+
+let isRaining = true
+isRaining
+    ? console.log("You need rain-coat.")
+    : console.log("No need for rain-coat.")
