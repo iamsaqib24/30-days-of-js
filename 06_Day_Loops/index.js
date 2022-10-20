bcdb510ecfc14ef2711244968fddb6884bbb4dca
Saqib <iamsaqib24@gmail.com>
2022-10-20 11:59:18 +0530

    for of loop

--- 06_Day_Loops/index.js
+++ 06_Day_Loops/index.js
@@ -58,3 +58,47 @@ do{
     console.log(j);
     j++;
 }while(j <= 1)
+
+
+// for of loop
+// We use for of loop for arrays. It is very hand way to iterate through an array if we are not interested in the index of each element in the array.
+console.log("for of loop");
+
+const numbers = [2, 5, 8, 11, 14];
+let sum = 0;
+for (const nums of numbers) {
+    console.log(nums);
+}
+
+for (const nums of numbers) {
+    sum = sum + nums;
+}
+console.log(sum);
+
+const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const newArr = []
+for(const country of countries){
+  newArr.push(country.toUpperCase())
+}
+console.log(countries);
+console.log(newArr);
+
+
+// break
+// Break is used to interrupt a loop.
+for(let i = 0; i <= 5; i++){
+    if(i == 3){
+      break
+    }
+    console.log(i);
+}           // 0 1 2
+// The above code stops if 3 found in the iteration process.
+
+// continue
+// We use the keyword continue to skip a certain iterations.
+for(let i = 0; i <= 5; i++){
+    if(i == 3){
+      continue
+    }
+    console.log(i)
+}           // 0 1 2 4 5
