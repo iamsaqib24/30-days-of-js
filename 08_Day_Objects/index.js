9410392c9533e3af16881004353fc3bf2b820459
Saqib <iamsaqib24@gmail.com>
2022-10-22 22:06:32 +0530

    getting values from object

--- 08_Day_Objects/index.js
+++ 08_Day_Objects/index.js
@@ -18,26 +18,68 @@
 
 // Local Scope,
 
-//scope.js
-let a = 'JavaScript' // is a global scope it will be found anywhere in this file
-let b = 10 // is a global scope it will be found anywhere in this file
-// Function scope
-function letsLearnScope() {
-  console.log(a, b) // JavaScript 10, accessible
-  let value = false
-// block scope
-  if (true) {
-    // we can access from the function and outside the function but
-    // variables declared inside the if will not be accessed outside the if block
-    let a = 'Python'
-    let b = 20
-    let c = 30
-    let d = 40
-    value = !value
-    console.log(a, b, c, value) // Python 20 30 true
+// //scope.js
+// let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+// let b = 10 // is a global scope it will be found anywhere in this file
+// // Function scope
+// function letsLearnScope() {
+//   console.log(a, b) // JavaScript 10, accessible
+//   let value = false
+// // block scope
+//   if (true) {
+//     // we can access from the function and outside the function but
+//     // variables declared inside the if will not be accessed outside the if block
+//     let a = 'Python'
+//     let b = 20
+//     let c = 30
+//     let d = 40
+//     value = !value
+//     console.log(a, b, c, value) // Python 20 30 true
+//   }
+//   // we can not access c because c's scope is only the if block
+//   console.log(a, b, value) // JavaScript 10 true
+// }
+// letsLearnScope()
+// console.log(a, b) // JavaScript 10, accessible
+
+
+// Objects,
+// Everything can be an object and objects do have properties and properties have values, so an object is a key value pair. The order of the key is not reserved, or there is no order. To create an object literal, we use two curly brackets.
+
+const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    getFullName: function() {
+      return `${this.firstName}${this.lastName}`
+    },
+    'phone number': '+3584545454545'
   }
-  // we can not access c because c's scope is only the if block
-  console.log(a, b, value) // JavaScript 10 true
-}
-letsLearnScope()
-console.log(a, b) // JavaScript 10, accessible
+
+  // accessing values using .
+  console.log(person.firstName)
+  console.log(person.lastName)
+  console.log(person.age)
+  console.log(person.location) // undefined
+
+  // value can be accessed using square bracket and key name
+  console.log(person['firstName'])
+  console.log(person['lastName'])
+  console.log(person['age'])
+  console.log(person['age'])
+  console.log(person['location']) // undefined
+
+  // for instance to access the phone number we only use the square bracket method
+  console.log(person['phone number'])
