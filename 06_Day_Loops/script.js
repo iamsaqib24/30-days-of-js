7ee135074633a054b38557daa2f174ddee940f37
Saqib <iamsaqib24@gmail.com>
2022-12-13 12:06:03 +0530

    push-operation

--- 06_Day_Loops/script.js
+++ 06_Day_Loops/script.js
@@ -1,11 +1,29 @@
 // Decrement
 
-for(let i = 10; i >= 5; i--) {
-    console.log(i);
-}
+// for(let i = 10; i >= 5; i--) {
+//     console.log(i);
+// }
 
 // Increment
 
-for(let i = 0; i <= 5; i++) {
-    console.log(i);
+// for(let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+// Adding elements to new array in uppercase
+
+let countries = ["India", "Singapore", "Canada", "Dubai", "Australia"];
+let newArray = [];
+
+// Before push operation
+console.log(countries);
+console.log(newArray);
+
+for(let i = 0; i < countries.length; i++) {
+    newArray.push(countries[i].toUpperCase());
 }
+
+// After push operation
+console.log(countries);
+console.log(newArray);
