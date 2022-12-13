23b613720b13d6d65dbca85b73eb94443253de3d
Saqib <iamsaqib24@gmail.com>
2022-12-13 12:11:56 +0530

    sum-of-array-elements

--- 06_Day_Loops/script.js
+++ 06_Day_Loops/script.js
@@ -13,17 +13,27 @@
 
 // Adding elements to new array in uppercase
 
-let countries = ["India", "Singapore", "Canada", "Dubai", "Australia"];
-let newArray = [];
+// let countries = ["India", "Singapore", "Canada", "Dubai", "Australia"];
+// let newArray = [];
 
 // Before push operation
-console.log(countries);
-console.log(newArray);
+// console.log(countries);
+// console.log(newArray);
 
-for(let i = 0; i < countries.length; i++) {
-    newArray.push(countries[i].toUpperCase());
-}
+// for(let i = 0; i < countries.length; i++) {
+//     newArray.push(countries[i].toUpperCase());
+// }
 
 // After push operation
-console.log(countries);
-console.log(newArray);
+// console.log(countries);
+// console.log(newArray);
+
+
+// Adding elements in the array
+
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(sum);
