220c453098c519da2d91970a50ae4ec9eed79290
Saqib <iamsaqib24@gmail.com>
2022-12-13 12:29:27 +0530

    for-of-loop

--- 06_Day_Loops/script.js
+++ 06_Day_Loops/script.js
@@ -40,10 +40,25 @@
 
 // Creating a new array based on the existing array
 
-const numbers = [1, 2, 3, 4, 5];
-const newArr = [];
-for(let i = 0; i < numbers.length; i++) {
-    newArr.push(numbers[i] ** 2);
-}
-console.log(numbers);
-console.log(newArr);
+// const numbers = [1, 2, 3, 4, 5];
+// const newArr = [];
+// for(let i = 0; i < numbers.length; i++) {
+//     newArr.push(numbers[i] ** 2);
+// }
+// console.log(numbers);
+// console.log(newArr);
+
+
+// for of loop
+
+// const numbers = [1, 2, 3, 4, 5];
+// for (const num of numbers) {
+//     console.log(num * num);
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (const num of numbers) {
+//     sum+= num;
+// }
+// console.log(sum);
